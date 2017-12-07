5b45798a5503574d99188aafb2462f4907c700f9
张鹏 <zhangpeng_bj@ysstech.com>
2017-12-07 17:56:44 +0800

    chang

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import { Input, Button } from 'antd';
 class Header extends React.Component{
+  handleChange (v) {
+    let values = v.target.value;
+    console.log(values)
+  }
   render(){
     return (
       <header>
         <section>
            <form>
               <label htmlFor="new-todo">备忘录</label>
-              <Input placeholder="新建事项(20字以内)"/>
+              <Input placeholder="新建事项(20字以内)" onChange={v => this.handleChange(v)}/>
               <div>
                 <Button type="default">添加</Button>
                 <Button type="default" icon="search">搜索</Button>
